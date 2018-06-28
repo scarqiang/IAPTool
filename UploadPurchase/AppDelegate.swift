302862de07e4a28a7092d138e31929df9bff31fe
bcxiaolang008 <360569102@qq.com>
2018-06-28 10:05:32 +0800

    初始化

---
+++ UploadPurchase/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  UploadPurchase
+//
+//  Created by Nemo on 2018/6/28.
+//  Copyright © 2018年 3K. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ UploadPurchase/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  UploadPurchase
+//
+//  Created by Nemo on 2018/6/28.
+//  Copyright © 2018年 3K. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
